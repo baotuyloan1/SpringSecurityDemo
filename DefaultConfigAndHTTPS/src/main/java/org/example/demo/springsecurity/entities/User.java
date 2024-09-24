2a436ca8eeca40a63de7fab56cc2a94c110cde0b
Nguyen Duc Bao <nguyenduc.bao@avenue-net.com>
2024-09-24 15:08:39 +0700

    MOved all code into DefaultConfigAndHTTPS

---
+++ DefaultConfigAndHTTPS/src/main/java/org/example/demo/springsecurity/entities/User.java
@@ -0,0 +1,29 @@
+package org.example.demo.springsecurity.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_authorities",
+            joinColumns = @JoinColumn(name = "\"userId\""),
+            inverseJoinColumns = @JoinColumn(name = "\"authorityId\"")
+    )
+    private Set<Authority> authorities;
+
+
+}
